07e1df36600dc85073f6554832fec0827fd274c3
ROSHAN-KANWAR <cpkroshan@gmail.com>
2025-03-25 06:20:45 +0530

    install laravel,setup database,add static files (bootstrap)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel -Ezystay Project</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+    <h1>Laravel Ezystay Project</h1>
+    </div>
+</body>
+</html>
